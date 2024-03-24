37bfae82e4280b9337e279657e24fb9afac7862f
TomsCelmins <164490325+TomsCelmins@users.noreply.github.com>
2024-03-24 13:07:03 +0000

    Board filled and its individual cells.

--- main.py
+++ main.py
@@ -8,7 +8,6 @@ cell_size = 30 # Size of the boards individual cell's, which would also mean the
 
 class Board:
   def __init__(self, start_loc_x, start_loc_y):
-
     # Contents of the board
     self.board_data = {
       0:["K","A","R","T","U","P","E","L","I","S"],
@@ -28,8 +27,8 @@ class Board:
     self.space = tk.Canvas(width = cell_size*11, height = cell_size*11, highlightthickness=1, highlightbackground="black")
     self.space.place(x = start_loc_x, y = start_loc_y)
     
-    y = cell_size
     x = cell_size
+    y = cell_size
     self.space.create_rectangle(cell_size-cell_size,cell_size-cell_size,cell_size,cell_size)
     for key in self.board_data:
       if key != 0:
@@ -42,7 +41,31 @@ class Board:
           self.space.create_text(x+cell_size/2,cell_size/2, text = element)
           x += cell_size
 
+class Board_Cell(Board):
+  def __init__(self, parent, x, y, row, column):
+    self.parent = parent
+    self.row = row
+    self.column = column
+    self.cell = self.parent.space.create_rectangle(x,y,x+cell_size,y+cell_size)
+    
+  def change_cell_color(self, color):
+    self.parent.space.itemconfigure(self.parent.board_data[self.row][self.column-1].cell, fill=color)
+
+def fill_board(board):
+  y = cell_size
+  for row in board.board_data:
+    x = cell_size
+    if len(board.board_data[row]) == 0:
+      for column in range(10):
+        board.board_data[row].append(Board_Cell(board, x, y, row, column))
+        x += cell_size
+      y += cell_size
+  
 board_1 = Board(0, 0)
 board_2 = Board(340, 0)
+
+fill_board(board_1)
+board_1.board_data[3][6].change_cell_color("red")
+
 
 tk.mainloop()
