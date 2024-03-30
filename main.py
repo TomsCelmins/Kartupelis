a6f670e09c4491e265e47d63ab5fb071ae21e656
TomsCelmins <164490325+TomsCelmins@users.noreply.github.com>
2024-03-30 11:05:06 +0000

    Organized, compacted and improved the code.

--- main.py
+++ main.py
@@ -1,14 +1,13 @@
 import tkinter as tk
 
 window = tk.Tk()
-window.title("Hello world")
 window.geometry("1000x500")
-
-cell_size = 30 # Size of the boards individual cell's, which would also mean the boards size.
 
 class Board:
   def __init__(self, start_loc_x, start_loc_y):
-    # Contents of the board
+    # Board Variables
+    cell_size = 30 # Size of the boards individual cell's, which would also mean the boards size.
+    self.selected = [] # Currently selected cell (row, column)
     self.board_data = {
       0:["K","A","R","T","U","P","E","L","I","S"],
       1:[],
@@ -23,74 +22,55 @@ class Board:
       10:[],
     }
 
-    # Creation of the boards static data
-    self.space = tk.Canvas(width = cell_size*11, height = cell_size*11, highlightthickness=1, highlightbackground="black")
+    # Creation of the board
+    self.space = tk.Canvas(width = cell_size*11, height = cell_size*11, highlightthickness=1, highlightbackground='black')
     self.space.place(x = start_loc_x, y = start_loc_y)
-    
-    x = cell_size
-    y = cell_size
-    self.space.create_rectangle(cell_size-cell_size,cell_size-cell_size,cell_size,cell_size)
+    self.space.bind_all('<Key>', self.cell_movement)
+
+    y = cell_size # For the row
     for key in self.board_data:
-      if key != 0:
-        self.space.create_rectangle(cell_size-cell_size,y,cell_size,y+cell_size)
-        self.space.create_text(cell_size/2,y+cell_size/2, text = f"{key}")
-        y += cell_size
-      else:
+      x = cell_size # For the column
+      if key == 0:
+        self.space.create_rectangle(cell_size-cell_size,cell_size-cell_size,cell_size,cell_size)
         for element in self.board_data[key]:
           self.space.create_rectangle(x,cell_size-cell_size,x+cell_size,cell_size)
           self.space.create_text(x+cell_size/2,cell_size/2, text = element)
           x += cell_size
+      else:
+        self.space.create_rectangle(cell_size-cell_size,y,cell_size,y+cell_size)
+        self.space.create_text(cell_size/2,y+cell_size/2, text = f"{key}")
+        for element in range(10):
+          cell = self.space.create_rectangle(x,y,x+cell_size,y+cell_size, fill='lightblue')
+          self.board_data[key].append(cell)
+          x += cell_size
+        y += cell_size
 
-class Board_Cell(Board):
-  def __init__(self, parent, x, y, row, column):
-    self.parent = parent
-    self.row = row
-    self.column = column
-    self.cell = self.parent.space.create_rectangle(x,y,x+cell_size,y+cell_size)
+  # For getting the first selected cell and changing it.
+  def selected_cell(self, row, column, starting_cell = False):
+    if not starting_cell:
+      self.space.itemconfigure(self.board_data[self.selected[0]][self.selected[1]], fill='lightblue')
+    self.space.itemconfigure(self.board_data[row][column], fill='green')
+    self.selected = [row, column]
+
+  def cell_movement(self, event):
+    pressed_key = event.keysym.lower()
     
-  def change_cell_color(self, color):
-    self.parent.space.itemconfigure(self.parent.board_data[self.row][self.column-1].cell, fill=color)
+    if pressed_key == 'w' and self.selected[0] > 1:
+      self.selected_cell(self.selected[0]-1, self.selected[1])
+    elif pressed_key == 'a' and self.selected[1] > 0:
+      self.selected_cell(self.selected[0], self.selected[1]-1)
+    elif pressed_key == 's' and self.selected[0] < 10:
+      self.selected_cell(self.selected[0]+1, self.selected[1])
+    elif pressed_key == 'd' and self.selected[1] < 9:
+      self.selected_cell(self.selected[0], self.selected[1]+1)
 
-def fill_board(board):
-  y = cell_size
-  for row in board.board_data:
-    x = cell_size
-    if len(board.board_data[row]) == 0:
-      for column in range(10):
-        board.board_data[row].append(Board_Cell(board, x, y, row, column))
-        x += cell_size
-      y += cell_size
-
+# Function to start the game
 def show_game():
   button.place_forget()
-  board_1 = Board(10, 10) # Placing the board down
-  
-  fill_board(board_1) # Filling the board with its cells
-  
-  # Modifying cells (board_data[row][column])
-  for i in range(3,9):
-    board_1.board_data[7][i].change_cell_color("red")
-  
-  board_1.board_data[6][2].change_cell_color("red")
-  board_1.board_data[6][9].change_cell_color("red")
-  board_1.board_data[2][4].change_cell_color("red")
-  board_1.board_data[3][4].change_cell_color("red")
-  board_1.board_data[2][7].change_cell_color("red")
-  board_1.board_data[3][7].change_cell_color("red")
-  
+  board_1 = Board(10, 10)
+  board_1.selected_cell(1,2, starting_cell = True)
   board_2 = Board(350, 10)
-  
-  fill_board(board_2)
-  
-  for i in range(3,9):
-    board_2.board_data[7][i].change_cell_color("blue")
-  
-  board_2.board_data[8][2].change_cell_color("blue")
-  board_2.board_data[8][9].change_cell_color("blue")
-  board_2.board_data[2][4].change_cell_color("blue")
-  board_2.board_data[3][4].change_cell_color("blue")
-  board_2.board_data[2][7].change_cell_color("blue")
-  board_2.board_data[3][7].change_cell_color("blue")
+  board_2.selected_cell(4,2, starting_cell = True)
 
 button = tk.Button(text="start game", command=show_game)
 button.place(x = 0,y = 0)
