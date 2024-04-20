d043869676b90e6255ac145e66d9b633252ba6e2
TomsCelmins <164490325+TomsCelmins@users.noreply.github.com>
2024-04-20 14:58:58 +0000

    Dev buttons for switching boards

--- main.py
+++ main.py
@@ -1,13 +1,17 @@
 import tkinter as tk
 
 window = tk.Tk()
-window.geometry("1000x500")
+window.geometry("1000x1000")
 
 class Board:
-  def __init__(self, start_loc_x, start_loc_y):
+  instances = []
+  cell_size = 30 # Size of the boards individual cell's, which would also mean the boards size.
+  
+  def __init__(self, start_loc_x, start_loc_y, cursor_row, cursor_column):
     # Board Variables
     cell_size = 30 # Size of the boards individual cell's, which would also mean the boards size.
-    self.selected = [] # Currently selected cell (row, column)
+    Board.instances.append(self)
+    self.selected = [cursor_row, cursor_column]
     self.board_data = {
       0:["K","A","R","T","U","P","E","L","I","S"],
       1:[],
@@ -22,11 +26,10 @@ class Board:
       10:[],
     }
 
-    # Creation of the board
+    # --- Creation of the board ---
     self.space = tk.Canvas(width = cell_size*11, height = cell_size*11, highlightthickness=1, highlightbackground='black')
     self.space.place(x = start_loc_x, y = start_loc_y)
-    self.space.bind_all('<Key>', self.cell_movement)
-
+    
     y = cell_size # For the row
     for key in self.board_data:
       x = cell_size # For the column
@@ -45,35 +48,54 @@ class Board:
           x += cell_size
         y += cell_size
 
-  # For getting the first selected cell and changing it.
-  def selected_cell(self, row, column):
-    self.selected = [row, column]
-    coords = self.space.coords(self.board_data[self.selected[0]][self.selected[1]])
-    self.cursor = self.space.create_oval(coords[0]+10,coords[1]+10,coords[2]-10,coords[3]-10, fill="black")
+    # The cursor
+    coords = self.space.coords(self.board_data[self.selected[0]][self.selected[1]-1])
+    self.cursor = self.space.create_oval(coords[0]+10,coords[1]+10,coords[2]-10,coords[3]-10, fill="red")
+    self.space.bind_all('<Key>', self.cell_movement)
 
+  def focus(self):
+    self.space.bind_all('<Key>', self.cell_movement)
+    self.space.itemconfigure(self.cursor, fill="black")
+    
   def cell_movement(self, event):
     pressed_key = event.keysym.lower()
     
     if pressed_key == 'w' and self.selected[0] > 1:
       self.space.move(self.cursor, 0, -30)
       self.selected = [self.selected[0]-1, self.selected[1]]
-    elif pressed_key == 'a' and self.selected[1] > 0:
+    elif pressed_key == 'a' and self.selected[1] > 1:
       self.space.move(self.cursor, -30, 0)
       self.selected = [self.selected[0], self.selected[1]-1]
     elif pressed_key == 's' and self.selected[0] < 10:
       self.space.move(self.cursor, 0, 30)
       self.selected = [self.selected[0]+1, self.selected[1]]
-    elif pressed_key == 'd' and self.selected[1] < 9:
+    elif pressed_key == 'd' and self.selected[1] < 10:
       self.space.move(self.cursor, 30, 0)
       self.selected = [self.selected[0], self.selected[1]+1]
+
+    # Testing commands for switching boards
+    match pressed_key:
+      case "v":
+        self.space.itemconfigure(self.cursor, fill="red")
+        Board.instances[0].focus()
+      case "b":
+        self.space.itemconfigure(self.cursor, fill="red")
+        Board.instances[1].focus()
+      case "n":
+        self.space.itemconfigure(self.cursor, fill="red")
+        Board.instances[2].focus()
+      case "m":
+        self.space.itemconfigure(self.cursor, fill="red")
+        Board.instances[3].focus()
+        
 
 # Function to start the game
 def show_game():
   button.place_forget()
-  board_1 = Board(10, 10)
-  board_1.selected_cell(1,2)
-  board_2 = Board(350, 10)
-  board_2.selected_cell(4,2)
+  board_1 = Board(10, 10, 1, 2)
+  board_2 = Board(350, 10, 4, 2)
+  board_3 = Board(10, 350, 4, 2)
+  board_4 = Board(350, 350, 4, 2)
 
 button = tk.Button(text="start game", command=show_game)
 button.place(x = 0,y = 0)
