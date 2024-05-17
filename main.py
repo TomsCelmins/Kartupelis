cf9d747014e9dfd13a4d9e740e69e92bfe3851c7
TomsCelmins <164490325+TomsCelmins@users.noreply.github.com>
2024-05-17 11:14:14 +0000

    Shooting, missing and winning.

--- main.py
+++ main.py
@@ -12,13 +12,16 @@ class Board:
     # Board Variables
     cell_size = 30 # Size of the boards individual cell's, which would also mean the boards size.
     Board.instances.append(self)
-    self.overlays = []
+    self.overlays = [] # Each element contains [(overlay_id), (ship_identifier)]
+    self.hit_ships = []
+    self.mode = "placing"
     # key == ship size, element == amount of ships
     self.ship_amounts = {
       2: 3,
       3: 2,
-      4: 1
+      4: 1,
     }
+    self.total_ships_left = 6
     self.cursor = {
       "row": 5,
       "column": 5,
@@ -67,10 +70,10 @@ class Board:
     # The cursor
     coords = self.board_data[self.cursor["row"]][self.cursor["column"]-1]
     self.cursor["overlay"] = self.space.create_oval(coords[0]+10,coords[1]+10,coords[2]-10,coords[3]-10, fill="red")
-    self.space.bind_all('<Key>', self.cell_movement)
+    self.space.bind_all('<Key>', self.key_press)
 
   def focus(self):
-    self.space.bind_all('<Key>', self.cell_movement)
+    self.space.bind_all('<Key>', self.key_press)
     self.space.itemconfigure(self.cursor["overlay"], fill="black")
 
   def placement_warning(self):
@@ -95,7 +98,7 @@ class Board:
         case "South":
           self.cursor["overlay"] = self.space.create_rectangle(coords[0],coords[1],coords[2],coords[3]+(self.cursor["ship_size"]-1)*cell_size, fill="black")
     
-  def cell_movement(self, event):
+  def key_press(self, event):
     pressed_key = event.keysym.lower()
     print(pressed_key)
     print(self.board_data[self.cursor["row"]][self.cursor["column"]-1])
@@ -114,121 +117,158 @@ class Board:
       elif pressed_key == 'd' and self.cursor["column"] < (11-self.cursor["ship_size"] if self.cursor["direction"] == "East" else 10):
         self.space.move(self.cursor["overlay"], 30, 0)
         self.cursor["column"] += 1
-
-    elif pressed_key == 'return':
-      if self.cursor["ship_size"] == 1: # We dont have 1 sized ships
-        return
-      # Safety checks to see if a nother ship is in the way
-      for cell in range(self.cursor["ship_size"]):
-        match self.cursor["direction"]:
-          case "West":
-            if self.board_data[self.cursor["row"]][self.cursor["column"]-cell-1][4] != 0.0:
-              self.placement_warning()
-              return
-          case "North":
-            if self.board_data[self.cursor["row"]-cell][self.cursor["column"]-1][4] != 0.0:
-              self.placement_warning()
-              return
-          case "East":
-            if self.board_data[self.cursor["row"]][self.cursor["column"]+cell-1][4] != 0.0:
-              self.placement_warning()
-              return
-          case "South": 
-            if self.board_data[self.cursor["row"]+cell][self.cursor["column"]-1][4] != 0.0:
-              self.placement_warning()
-              return
-      # Reference that these cells currently have a ship on it
-      ship_identifier = self.cursor["ship_size"]*10+self.ship_amounts[self.cursor["ship_size"]]
-      new_overlay = self.space.create_rectangle(self.space.coords(self.cursor["overlay"]), fill='black')
-      self.overlays.append([new_overlay, ship_identifier])
-      for cell in range(self.cursor["ship_size"]):
-        match self.cursor["direction"]:
-          case "West":
-            self.board_data[self.cursor["row"]][self.cursor["column"]-cell-1][4] = ship_identifier
-          case "North":
-            self.board_data[self.cursor["row"]-cell][self.cursor["column"]-1][4] = ship_identifier
-          case "East":
-            self.board_data[self.cursor["row"]][self.cursor["column"]+cell-1][4] = ship_identifier
-          case "South":
-            self.board_data[self.cursor["row"]+cell][self.cursor["column"]-1][4] = ship_identifier
-      # Remove 1 ship from the amount
-      self.ship_amounts[self.cursor["ship_size"]] -= 1
-      if self.ship_amounts[self.cursor["ship_size"]] == 0:
-        self.cursor["ship_size"] = 1
-        self.redraw_ship()
-
-    elif pressed_key == 'backspace':
-      if self.overlays == []: # Nothing to delete if theres nothing on the board
-        return
-      object = self.overlays[len(self.overlays)-1]
-      print(object)
-      for row in self.board_data:
-        if row == 0:
-          continue
-        for cell in self.board_data[row]:
-          if cell[4] == object[1]:
-            cell[4] = 0.0
-
-      self.space.itemconfigure(object[0], state='hidden')
-      self.ship_amounts[object[1]//10] += 1
-      self.overlays.remove(object)
-      
-    # Changing Direction
-    elif pressed_key in {'q', 'e'}:
-      # Early return if invalid
-      match self.cursor["direction"]:
-        case "West":
-          if (self.cursor["row"]-self.cursor["ship_size"] < 0 if pressed_key == "e" else self.cursor["row"]+self.cursor["ship_size"] > 11):
-            self.placement_warning()
-            return
-        case "North":
-          if (self.cursor["column"]-self.cursor["ship_size"] < 0 if pressed_key == "q" else self.cursor["column"]+self.cursor["ship_size"] > 11):
-            self.placement_warning()
-            return
-        case "East":
-          if (self.cursor["row"]-self.cursor["ship_size"] < 0 if pressed_key == "q" else self.cursor["row"]+self.cursor["ship_size"] > 11):
-            self.placement_warning()
-            return
-        case "South":
-          if (self.cursor["column"]-self.cursor["ship_size"] < 0 if pressed_key == "e" else self.cursor["column"]+self.cursor["ship_size"] > 11):
-            self.placement_warning()
-            return
-    
-      directions = ["West", "North", "East", "South"]
+    if self.mode == "placing":
       match pressed_key:
-        case "q":
-          self.cursor["direction"] = directions[directions.index(self.cursor["direction"])-1]
-        case "e":
-          directions_reversed = directions[::-1]
-          self.cursor["direction"] = directions_reversed[directions_reversed.index(self.cursor["direction"])-1]
-      self.redraw_ship()
-            
-    # Selecting ship size
-    elif pressed_key in {"1", "2", "3", "4"}:
-      if int(pressed_key) != 1 and self.ship_amounts[int(pressed_key)] == 0: # Cant place ships if there are none left
-        self.placement_warning()
-        return
+        # Action key
+        case 'f':
+          if self.cursor["ship_size"] == 1: # We dont have 1 sized ships
+            return
+          # Safety checks to see if a nother ship is in the way
+          for cell in range(self.cursor["ship_size"]):
+            match self.cursor["direction"]:
+              case "West":
+                if self.board_data[self.cursor["row"]][self.cursor["column"]-cell-1][4] != 0.0:
+                  self.placement_warning()
+                  return
+              case "North":
+                if self.board_data[self.cursor["row"]-cell][self.cursor["column"]-1][4] != 0.0:
+                  self.placement_warning()
+                  return
+              case "East":
+                if self.board_data[self.cursor["row"]][self.cursor["column"]+cell-1][4] != 0.0:
+                  self.placement_warning()
+                  return
+              case "South": 
+                if self.board_data[self.cursor["row"]+cell][self.cursor["column"]-1][4] != 0.0:
+                  self.placement_warning()
+                  return
+          # Reference that these cells currently have a ship on it
+          ship_identifier = self.cursor["ship_size"]*10+self.ship_amounts[self.cursor["ship_size"]]
+          new_overlay = self.space.create_rectangle(self.space.coords(self.cursor["overlay"]), fill='black')
+          self.overlays.append([new_overlay, ship_identifier])
+          for cell in range(self.cursor["ship_size"]):
+            match self.cursor["direction"]:
+              case "West":
+                self.board_data[self.cursor["row"]][self.cursor["column"]-cell-1][4] = ship_identifier
+              case "North":
+                self.board_data[self.cursor["row"]-cell][self.cursor["column"]-1][4] = ship_identifier
+              case "East":
+                self.board_data[self.cursor["row"]][self.cursor["column"]+cell-1][4] = ship_identifier
+              case "South":
+                self.board_data[self.cursor["row"]+cell][self.cursor["column"]-1][4] = ship_identifier
+          # Remove 1 ship from the amount
+          self.ship_amounts[self.cursor["ship_size"]] -= 1
+          if self.ship_amounts[self.cursor["ship_size"]] == 0:
+            self.cursor["ship_size"] = 1
+            self.redraw_ship()
+    
+        # Delete previously placed ship
+        case 'backspace':
+          if self.overlays == []: # Nothing to delete if theres nothing on the board
+            return
+          object = self.overlays[len(self.overlays)-1]
+          for row in self.board_data:
+            if row == 0:
+              continue
+            for cell in self.board_data[row]:
+              if cell[4] == object[1]:
+                cell[4] = 0.0
+    
+          self.space.itemconfigure(object[0], state='hidden')
+          self.ship_amounts[object[1]//10] += 1
+          self.overlays.remove(object)
+
+        # Confirming your ship placement
+        case 'return':
+          # Safety check if all ships have been placed
+          for num in self.ship_amounts:
+            if self.ship_amounts[num] > 0:
+              return
+
+          for object in self.overlays:
+            self.space.itemconfigure(object[0], state='hidden')
+
+          self.mode = "shooting"
+          
+        # Change cardinal direction
+        case 'q'|'e':
+          # Early return if invalid
+          match self.cursor["direction"]:
+            case "West":
+              if (self.cursor["row"]-self.cursor["ship_size"] < 0 if pressed_key == "e" else self.cursor["row"]+self.cursor["ship_size"] > 11):
+                self.placement_warning()
+                return
+            case "North":
+              if (self.cursor["column"]-self.cursor["ship_size"] < 0 if pressed_key == "q" else self.cursor["column"]+self.cursor["ship_size"] > 11):
+                self.placement_warning()
+                return
+            case "East":
+              if (self.cursor["row"]-self.cursor["ship_size"] < 0 if pressed_key == "q" else self.cursor["row"]+self.cursor["ship_size"] > 11):
+                self.placement_warning()
+                return
+            case "South":
+              if (self.cursor["column"]-self.cursor["ship_size"] < 0 if pressed_key == "e" else self.cursor["column"]+self.cursor["ship_size"] > 11):
+                self.placement_warning()
+                return
         
-      match self.cursor["direction"]:
-        case "West":
-          if self.cursor["column"]-int(pressed_key) < 0:
+          directions = ["West", "North", "East", "South"]
+          match pressed_key:
+            case "q":
+              self.cursor["direction"] = directions[directions.index(self.cursor["direction"])-1]
+            case "e":
+              directions_reversed = directions[::-1]
+              self.cursor["direction"] = directions_reversed[directions_reversed.index(self.cursor["direction"])-1]
+          self.redraw_ship()
+                
+        # Select ship size
+        case "1"|"2"|"3"|"4":
+          if int(pressed_key) != 1 and self.ship_amounts[int(pressed_key)] == 0: # Cant place ships if there are none left
             self.placement_warning()
             return
-        case "North":
-          if self.cursor["row"]-int(pressed_key) < 0:
-            self.placement_warning()
-            return
-        case "East":
-          if self.cursor["column"]+int(pressed_key) > 11:
-            self.placement_warning()
-            return
-        case "South":
-          if self.cursor["row"]+int(pressed_key) > 11:
-            self.placement_warning()
-            return
-      
-      self.cursor["ship_size"] = int(pressed_key)
-      self.redraw_ship()
+            
+          match self.cursor["direction"]:
+            case "West":
+              if self.cursor["column"]-int(pressed_key) < 0:
+                self.placement_warning()
+                return
+            case "North":
+              if self.cursor["row"]-int(pressed_key) < 0:
+                self.placement_warning()
+                return
+            case "East":
+              if self.cursor["column"]+int(pressed_key) > 11:
+                self.placement_warning()
+                return
+            case "South":
+              if self.cursor["row"]+int(pressed_key) > 11:
+                self.placement_warning()
+                return
+          
+          self.cursor["ship_size"] = int(pressed_key)
+          self.redraw_ship()
+    else:
+      match pressed_key:
+        case 'f':
+          cell = self.board_data[self.cursor["row"]][self.cursor["column"]-1]
+          if cell[4] != 0.0 and cell[4] != 'shot':
+            self.space.create_line(cell[0],cell[1],cell[2],cell[3])
+            self.space.create_line(cell[0]+cell_size,cell[1],cell[2]-cell_size,cell[3])
+
+            # Referencing the ship part that was hit + checking if the entire ship has been destroyed
+            self.hit_ships.append(cell[4])
+            if self.hit_ships.count(cell[4]) == cell[4]//10:
+              for object in self.overlays:
+                if object[1] == cell[4]:
+                  self.space.itemconfigure(object[0], state='normal', fill='red')
+              self.total_ships_left -= 1
+              # End game if all ships have been destroyed
+              if self.total_ships_left == 0:
+                print("GAME END!")
+              
+            cell[4] = "shot"
+          else:
+            cell[4] = "shot"
+            self.space.create_oval(cell[0]+5,cell[1]+5,cell[2]-5,cell[3]-5)
 
     # Dev commands for switching boards
     match pressed_key:
